f7f457dcbbe8659fc9cf30e0fb8e72f2b2bda001
JoshLampen <joshlampen2@gmail.com>
2020-04-28 23:26:10 +0000

    Completed first draft

---
+++ letterPositions.js
@@ -0,0 +1,36 @@
+// FUNCTION IMPLEMENTATION
+const assertArraysEqual = (array1, array2) => {
+  // Check for identical array length
+  if (array1.length !== array2.length) {
+    console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+    return;
+  }
+  // Check for identical elements
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      console.log(`🛑🛑🛑 Assertion Failed: [${array1}] !== [${array2}]`);
+      return;
+    }
+  }
+  console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`);
+};
+
+const letterPositions = sentence => {
+  const results = {}; // Initialize an output object
+  for (let i = 0; i < sentence.length; i++) { // Loop through each character of the string
+    if (sentence[i] !== " ") { // Make sure to avoid counting spaces
+      if (results[sentence[i]]) { // If the letter is already a key in the object, add the index position to the key's array
+        results[sentence[i]].push(i);
+      } else { // If not, initialize a key and value for the letter
+        results[sentence[i]] = [i];
+      }
+    }
+  }
+  return results;
+};
+
+// TEST CODE
+assertArraysEqual(letterPositions("hello")["h"], [0]);
+assertArraysEqual(letterPositions("hello")["e"], [1]);
+assertArraysEqual(letterPositions("hello")["l"], [2, 3]);
+assertArraysEqual(letterPositions("hello")["o"], [4]);
