c6383d8e15453308e8c057628a6e919b62d25c41
JoshLampen <joshlampen2@gmail.com>
2020-05-04 13:55:16 +0000

    Separated test code and placed in a new test folder; added import/export functionality

--- head.js
+++ head.js
@@ -1,24 +1,6 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (typeof actual === "number" && typeof expected === "number") {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  } else {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-    }
-  }
-};
-
 const head = function(array) {
   return array[0];
 };
 
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
