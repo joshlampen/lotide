6db95ddc32fff0b0f859b86206d57095309d6ca4
JoshLampen <joshlampen2@gmail.com>
2020-05-04 16:15:08 +0000

    Separated test code to separate file and added chai functionality

--- tail.js
+++ tail.js
@@ -1,20 +1,4 @@
 // FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (typeof actual === "number" && typeof expected === "number") {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-    }
-  } else {
-    if (actual === expected) {
-      console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
-    } else {
-      console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
-    }
-  }
-};
-
 const tail = function(array) {
   let newArray = [];
   for (let i = 1; i < array.length; i++) { // for loop ensures the original array is not modified
@@ -23,12 +7,4 @@ const tail = function(array) {
   return newArray;
 };
 
-// TEST CODE
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
